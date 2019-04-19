5a83242431b14042577185cf19594d2e4b9f7895
NYRIAstronomy <47816759+NYRIAstronomy@users.noreply.github.com>
2019-04-19 05:07:24 -0700

    Update send_form_email.php

--- send_form_email.php
+++ send_form_email.php
@@ -2,7 +2,7 @@
 if(isset($_POST['email'])) {
  
     // EDIT THE 2 LINES BELOW AS REQUIRED
-    $email_to = "astro.instrumentation@gmail.com";
+    $email_to = "perera@mpia.de";
     $email_subject = "Your email subject line";
  
     function died($error) {
