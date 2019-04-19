2f5716b8f3ff282dd9a726d380221e7ebb69c090
NYRIAstronomy <47816759+NYRIAstronomy@users.noreply.github.com>
2019-04-19 04:54:59 -0700

    Update and rename contact.html to contact.php

--- contact.php
+++ contact.php
@@ -6,23 +6,49 @@ title:  "Contact"
 permalink: /contact/
 ---
 <?php
-if($_POST["message"]) {
-    mail("your@email.address", "Form to email message", $_POST["message"], "From: an@email.address");
+
+if($_POST["submit"]) {
+    $recipient="your@email.address";
+    $subject="Form to email message";
+    $sender=$_POST["sender"];
+    $senderEmail=$_POST["senderEmail"];
+    $message=$_POST["message"];
+
+    $mailBody="Name: $sender\nEmail: $senderEmail\n\n$message";
+
+    mail($recipient, $subject, $mailBody, "From: $sender <$senderEmail>");
+
+    $thankYou="<p>Thank you! Your message has been sent.</p>";
 }
-?>
 
-<h1>Contact</h1>
-Place contact form here ...
+?><!DOCTYPE html>
 
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Contact form to email</title>
+</head>
 
+<body>
 
+    <?=$thankYou ?>
 
+    <form method="post" action="contact.php">
+        <label>Name:</label>
+        <input name="sender">
 
+        <label>Email address:</label>
+        <input name="senderEmail">
 
-<form method="post" action="contact.php">
-    <textarea name="message"></textarea>
-    <input type="submit">
-</form>
+        <label>Message:</label>
+        <textarea rows="5" cols="20" name="message"></textarea>
+
+        <input type="submit" name="submit">
+    </form>
+
+</body>
+
+</html>
 
 
 <!-- <head>
